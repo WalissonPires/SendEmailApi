943cbe2261601bea9d62442a73feeaf801f188ac
Walisson Pires R. Miranda <walissonpiresgv@hotmail.com>
2024-12-09 15:33:39 -0300

    Enable Cors ...
    Setup flyio deploy

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
+require('dotenv').config();
 const nodemailer = require('nodemailer');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send({ message: 'ON' }));
+app.get('/', (req, res) => res.send({ message: 'v1.0.0' }));
 
-app.post('/email', (req, res) => {
+app.post('/', handleSendEmail);
+app.post('/email', handleSendEmail);
+
+function handleSendEmail(req, res) {
 
     try {
-        const email = {
-            from: req.body.from,
-            to: req.body.to,
-            subject: req.body.subject,
-            content: req.body.content
-        };
-
         const transporter = nodemailer.createTransport({
             service: req.body.provider || 'gmail',
             auth: {
@@ -29,7 +28,7 @@ app.post('/email', (req, res) => {
             to: req.body.to,
             subject: req.body.subject,
             html: req.body.content
-        }
+        };
 
         transporter.sendMail(emailMessage, (error) => {
 
@@ -49,7 +48,7 @@ app.post('/email', (req, res) => {
         console.error(error);
         res.status(422).send({ message: 'Fail send email' })
     }
-});
+}
 
 const PORT = process.env.PORT || '5010';
 app.listen(PORT, () => console.log('Server on port ' + PORT));
